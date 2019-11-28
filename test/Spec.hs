073f2f586a316d8e4d447720192f0da4c352e7da
Rafał Szczerski <rafal.szczerski@gmail.com>
2019-11-28 18:27:11 +0100

    Instance laws checks for finite slists; also, fix for the concatenation law (mconcat ≡ foldr mappend mempty)

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,4 +1,27 @@
 module Main (main) where
 
+import Slist as  SL
+import           Hedgehog
+import           Hedgehog.Main (defaultMain)
+import           Hedgehog.Classes
+import qualified Hedgehog.Gen as Gen
+import qualified Hedgehog.Range as Range
+
 main :: IO ()
-main = putStrLn ("Test suite not yet implemented" :: String)
+main = defaultMain $
+  lawsCheck <$>
+    [ functorLaws genFiniteSlist
+    , applicativeLaws genFiniteSlist
+    , monadLaws genFiniteSlist
+    , foldableLaws genFiniteSlist
+    , traversableLaws genFiniteSlist
+    , alternativeLaws genFiniteSlist
+    , semigroupLaws (genFiniteSlist genInt)
+    , monoidLaws (genFiniteSlist genInt)
+    ]
+
+genInt :: Gen Int
+genInt = Gen.int (Range.linear 0 100)
+
+genFiniteSlist :: Gen a -> Gen (Slist a)
+genFiniteSlist gen = slist <$> Gen.list (Range.linear 0 100) gen
